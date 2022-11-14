3a410d18b0680acfdd56ca0dad926d6725106a2a
julianramirezmeight <julian.ramirez@macheight.com>
2022-11-14 11:44:56 -0500

    xd

--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -20,6 +20,24 @@ class FileSerializer(serializers.ModelSerializer):
 
 
 class ReportSerializer(serializers.ModelSerializer):
+    quimico_report = serializers.ReadOnlyField(
+        source='report.quimico_report',
+        required=False,
+        allow_null=True
+    )
+
+    bio_y_cor_report = serializers.ReadOnlyField(
+        source='report.bio_y_cor_report',
+        required=False,
+        allow_null=True
+    )
+
+    ord_y_rec_report = serializers.ReadOnlyField(
+        source='report.ord_y_rec_report',
+        required=False,
+        allow_null=True
+    )
+
     class Meta:
         model = Report
         fields = [
